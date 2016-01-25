e04e7979c66d7318efbb3eb82cf0c897bf8ff691
Grant Jones <gtj4@pitt.edu>
2016-01-25 17:56:11 -0500

    Added some instructions for using git shell

--- Test.java
+++ Test.java
@@ -2,6 +2,10 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Test!");
+		System.out.println("All of this assumes the file has been saved to \Documents\Github\file");
+		System.out.println("To open a java file on Notepad++ using git shell type:     start notepad++ .\filename.java");
+		System.out.println("To commit changes(to make them appear... to save them) type git commit -a -m "comment" ");
+		System.out.println("To have the changes appear on the file online we have to push it. Type: git push origin master");
     }
 
 }
